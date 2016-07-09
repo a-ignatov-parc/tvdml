0a1e98d75db56d41454d6609ebf2b3c41dc31929
Anton Ignatov <abietis@gmail.com>
2016-07-09 05:54:04 +0300

    Implemented jsx to uvdom transpiling

--- src/jsx.js
+++ src/jsx.js
@@ -1,3 +1,25 @@
 export default function jsxToUVDOM(tag, attrs, ...children) {
-	console.log('jsx', tag, attrs, children);
+	const node = {tag};
+
+	if (attrs) {
+		Object
+			.keys(attrs)
+			.forEach(name => {
+				if (typeof(attrs[name]) === 'function') {
+					node.events || (node.events = {});
+					node.events[name] = attrs[name];
+				} else {
+					node.attrs || (node.attrs = {});
+					node.attrs[name] = attrs[name];
+				}
+			});
+	}
+
+	if (children.length === 1) {
+		node.children = children[0];
+	} else if (children.length > 1) {
+		node.children = children;
+	}
+
+	return node;
 }
