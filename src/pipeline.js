6b581a32d5b5cf8ad8d6c4645377ec2cc1ccd5f9
Anton Ignatov <abietis@gmail.com>
2016-10-30 20:30:43 +0300

    Switched to new pipeline implementation

--- src/pipeline.js
+++ src/pipeline.js
@@ -5,52 +5,12 @@ import {noop} from './utils';
 import Pipeline from './pipeline/simple';
 import PassthroughPipeline from './pipeline/passthrough';
 
-export function createPipeline2(options) {
+export function createPipeline(options) {
 	return new Pipeline(options);
 }
 
-export function createPassThroughPipeline2(options) {
-	return new PassthroughPipeline(options);
-}
-
-export function createPipeline(options = {}, params) {
-	const {
-		chain = [],
-		passthrough,
-	} = params || {};
-
-	const pipeline = (...args) => (payload) => {
-		return chain
-			.reduce((target, handler, i) => {
-				return target
-					.then((options.onSinkStep || onSinkStep).bind(null, i))
-					.then(handler);
-			}, Promise.resolve(assign({}, payload, {args})))
-			.catch(error => console.error(error));
-	};
-
-	return assign(pipeline, options.extend, {
-		pipe(handler) {
-			if (typeof(handler.pipe) === 'function') {
-				handler = handler();
-			}
-
-			if (passthrough) {
-				chain.push(handler);
-				return this;
-			}
-
-			return createPipeline(options, {chain: chain.concat(handler)});
-		},
-
-		sink(payload) {
-			return pipeline()(payload);
-		},
-	});
-}
-
 export function createPassThroughPipeline(options) {
-	return createPipeline(options, {passthrough: true});
+	return new PassthroughPipeline(options);
 }
 
 export function passthrough(handler = noop()) {
@@ -62,8 +22,4 @@ export function passthrough(handler = noop()) {
 				return assign({}, payload, extra);
 			});
 	};
-}
-
-function onSinkStep(step, payload) {
-	return payload;
 }
